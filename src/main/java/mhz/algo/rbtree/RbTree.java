8831eeca4c39564499ddfe3d6dd78887ace7a22d
fripSideMeng <2254257145@qq.com>
2020-09-09 22:18:26 -0400

    Node now supports integers only, tree insertion left-left, left-right implemented

--- src/main/java/mhz/algo/rbtree/RbTree.java
+++ src/main/java/mhz/algo/rbtree/RbTree.java
@@ -1,4 +1,115 @@
 package mhz.algo.rbtree;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RbTree {
+    private Set<Integer> inserted;
+    private RbTreeNode overallRoot;
+
+    public RbTree() {
+        inserted = new HashSet<>();
+    }
+
+    public int size() {
+        return inserted.size();
+    }
+
+    public boolean contains(RbTreeNode node) {
+        return inserted.contains(node.data);
+    }
+
+    public void insert(RbTreeNode node) {
+        if (node == null || this.contains(node)) {
+            throw new IllegalArgumentException("Node is duplicate or empty");
+        }
+        if (inserted.size() == 0) {
+            overallRoot = new RbTreeNode(node.data);
+            overallRoot.red = false; // Always paint root as black
+        } else {
+            insertHelper(node); // Do a normal BST insertion
+            repaintInsertion(node); // Restore RB tree invariant
+        }
+        inserted.add(node.data);
+    }
+
+    private void insertHelper(RbTreeNode node) {
+        RbTreeNode curr = overallRoot;
+        while (curr != null) {
+            if (curr.data < node.data) {
+                if (curr.right == null) {
+                    node.parent = curr;
+                    curr.right = node;
+                    return;
+                } else {
+                    curr = curr.right;
+                }
+            } else {
+                if (curr.left == null) {
+                    node.parent = curr;
+                    curr.left = node;
+                    return;
+                } else {
+                    curr = curr.left;
+                }
+            }
+        }
+    }
+
+    private void repaintInsertion(RbTreeNode node) {
+        if (node == null || node.parent == null || node.parent.parent == null) {
+            return;
+        }
+        if (node.parent.red) {
+            RbTreeNode uncle = node.parent.equals(node.parent.parent.left)
+                                ? node.parent.parent.right : node.parent.parent.left;
+            if (uncle != null) {
+                if (uncle.red) {
+                    node.parent.red = false;
+                    uncle.red = false;
+                    repaintInsertion(node.parent.parent); // Recursively repaint grandfather
+                } else { // Uncle is black
+                    if (node.parent.equals(node.parent.parent.left)
+                            && node.equals(node.parent.left)) { // Left-left case
+                        RbTreeNode tmp = node.parent.parent;
+                        node.parent.parent = node.parent.parent.parent;
+                        if (node.parent.parent != null) {
+                            if (tmp.equals(node.parent.parent.left)) {
+                                node.parent.parent.left = node.parent;
+                            } else if (tmp.equals(node.parent.parent.right)) {
+                                node.parent.parent.right = node.parent;
+                            }
+                        }
+                        tmp.left = node.parent.right;
+                        node.parent.right = tmp; // Rotation finished
+                        node.parent.red = false;
+                        node.parent.right.red = true; // Repaint finished for the original grandparent
+                    } else if (node.parent.equals(node.parent.parent.left)
+                           && node.equals((node.parent.right))) { // Left-right case
+                        RbTreeNode tmp = node.parent;
+                        node.parent.parent.left = node;
+                        node.parent = node.parent.parent;
+                        tmp.right = node.left;
+                        node.left = tmp;
+                        tmp.parent = node; // Finished left rotation of parent node
+                        tmp = node.parent;
+                        node.parent = node.parent.parent;
+                        if (node.parent != null) {
+                            if (tmp.equals(node.parent.left)) {
+                                node.parent.left = node;
+                            } else if (tmp.equals(node.parent.right)) {
+                                node.parent.right = node;
+                            }
+                        }
+                        tmp.left = node.right;
+                        node.right = tmp;
+                        node.left.red = true;
+                        node.right.red = true; // Repaint finished for the original grandparent
+                    }
+                }
+            } else {
+                node.parent.red = false;
+            }
+        }
+    }
 }
--- src/main/java/mhz/algo/rbtree/RbTreeNode.java
+++ src/main/java/mhz/algo/rbtree/RbTreeNode.java
@@ -1,30 +1,19 @@
 package mhz.algo.rbtree;
 
-class RbTreeNode<T> {
-    public final T data;
+public class RbTreeNode {
+    public final int data;
     public boolean red;
     public RbTreeNode left;
     public RbTreeNode right;
     public RbTreeNode parent;
 
     // Create a single new node with red mark
-    RbTreeNode(T data) {
-        this.data = data;
-        this.red = true;
-        this.parent = null;
-        this.left = null;
-        this.right = null;
-    }
-
-    // Create a new node with given parent with red mark
-    RbTreeNode(T data, RbTreeNode parent) {
-        this.data = data;
-        this.red = true;
-        this.parent = parent;
+    public RbTreeNode(int data) {
+        this(data, null, null, null);
     }
 
     // Create a new node with given positions with red mark
-    RbTreeNode(T data, RbTreeNode left, RbTreeNode right, RbTreeNode parent) {
+    public RbTreeNode(int data, RbTreeNode left, RbTreeNode right, RbTreeNode parent) {
         this.data = data;
         this.red = true;
         this.left = left;
